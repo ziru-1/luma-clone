7fcd76b99091abde076b736c5e53bdc1458bee0e
zed <zedcaoile@gmail.com>
2026-07-06 19:33:57 +0800

    fix: enable CORS for local dev

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -5,6 +5,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5173',
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(process.env.PORT ?? 3000);
 }
